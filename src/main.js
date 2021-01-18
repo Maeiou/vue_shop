8d6d2a4dd13f3a722e709f66b126e81d289a42a8
Maeiou <mrtlalala@qq.com>
2021-01-18 21:29:52 +0800

    完成了登录功能

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+
 import './plugins/element.js'
+import './assets/CSS/global.css'
+
+Vue.prototype.$http =axios  //配置请求根路径
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
 
 Vue.config.productionTip = false
+
 
 new Vue({
   router,
